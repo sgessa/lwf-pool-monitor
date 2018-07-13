e4a9f10a7f49805fbb8815ef2d46fc65ec9a623f
Stefano Gessa <stefano@gessa.net>
2018-07-13 17:46:53 +0700

    Bump version

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION="0.1.0"
+VERSION="0.1.1"
 RELEASE_URL="https://github.com/sgessa/lwf-pool-monitor/releases/download/v$VERSION/lwf-pool-monitor-linux-amd64.tar.gz"
 DST="$HOME/lwf-pool-monitor.tar.gz"
 
